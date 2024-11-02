fd88da3e03ae9628869d96351c074f444741d60b
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-11-02 17:10:33 -0600

    hooks @

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
 import { TODO_DATA } from '../assets/todo';
 import { NTodo } from './models/todo.model';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoComponent, CommonModule],
+  imports: [RouterOutlet, TodoComponent, CommonModule, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  todoData = TODO_DATA;
+  todoData = TODO_DATA[0];
+  // todoData = TODO_DATA.filter(item => item.id < 2);
   // todoData: NTodo.TodoData[] = []
+
+  // @ViewChild('todoRef', { read: ElementRef }) todo?: ElementRef;
+  @ViewChildren(TodoComponent, { read: ElementRef }) todo?: QueryList<ElementRef>;
+
+
+  constructor() { }
 
   getTodoInfo(val: NTodo.TodoData) {
     console.log(val);
@@ -26,6 +34,19 @@ export class AppComponent {
   }
 
   orderData() {
-    this.todoData.sort((a, b) => a.priority - b.priority)
+    // this.todoData.sort((a, b) => a.priority - b.priority)
+  }
+
+  change() {
+    // this.todo?.changes.subscribe(values => {
+    //   console.log(values);
+    // })
+
+
+    this.todoData = { ...this.todoData, description: 'Descripcion para ver si funciona ngOnChanges.' }
+  }
+
+  addTodo() {
+    // this.todoData = TODO_DATA.filter(item => item.id < 5)
   }
 }
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, LOCALE_ID, Output } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, ContentChildren, DoCheck, ElementRef, EventEmitter, Input, LOCALE_ID, OnChanges, OnDestroy, OnInit, Output, QueryList, SimpleChanges, ViewChild } from '@angular/core';
 import { NTodo } from '../../models/todo.model';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
+import { InputComponent } from '../../components/input/input.component';
 registerLocaleData(es)
 
 @Component({
@@ -16,13 +17,67 @@ registerLocaleData(es)
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
-export class TodoComponent {
+export class TodoComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  constructor() {
+    console.log("constructor");
+
+  }
+
+  // Escucha cambios que suceden en decorador @Input()
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges");
+
+  }
+
+  //Hacer operaciones que no se pudieron hacer en el constructor
+  //Hacer operaciones básicas, inyectar dependencias...
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  // Escucha todos los cambios que suceden en la aplicación
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+
+  }
+
+  // Se ejecuta cuando se ha leido/verificado por primera vez el contenido proyectado
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+
+  }
+
+  // Se ejecuta cada que haya un cambio en la aplicación
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+
+  }
+
+  // Se tiene acceso al template
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+
+  }
+
+  // Se ejecuta cuando haya cambios en la aplicación
+  // Se usa solo para cambios especificos en la plantilla o contenido
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+
+  }
+
+  // Se ejecuta cuando se elimia el componente
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+
+  }
+
   @Input({ required: true }) todoData!: NTodo.TodoData;
-  @Input() first!: boolean;
-  @Input() last!: boolean;
-  @Input() odd!: boolean;
-  @Input() even!: boolean;
   @Output() onClickIcon = new EventEmitter<NTodo.TodoData>();
+  @ContentChildren(InputComponent, { read: ElementRef }) projectedContent?: QueryList<ElementRef>;
 
   get priority(): string {
     switch (this.todoData.priority) {
@@ -48,5 +103,11 @@ export class TodoComponent {
     }
 
     return NTodo.RangeText.HIGH;
+  }
+
+  selectContent() {
+    const elements = this.projectedContent?.map(item => item)
+    console.log(elements);
+
   }
 }
